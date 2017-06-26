076eacb4abbed0fb7aa259b6b1b9a7f274d662ec
Young Ryu C <young.ryu@nutanix.com>
2017-06-26 15:03:09 -0700

    Add project files.

---
+++ ParseJSONData/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+//using CSVFile;
+
+namespace ParseJSONData
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            // Parse json data from workday json data
+            JArray jsonData = JArray.Parse(File.ReadAllText(@"C:\INT_Okta_Worker_Import.json"));
+
+            // Deserialize Json Data
+            List<WorkdayAttributes> result = JsonConvert.DeserializeObject<List<WorkdayAttributes>>(jsonData.ToString());
+
+            var export = new CsvExport();
+
+            // Export csv file with Workday Users
+            for (int i = 0; i < result.Count; i++)
+            {
+                export.AddRow();
+                export["UPN"] = result[i].primaryWorkEmail;
+                export["Name"] = result[i].Preferred_Name;
+                export["Employe ID"] = result[i].Employee_ID;
+                export["Manager"] = result[i].Manager_Name;
+                export["Manager Email"] = result[i].Managers_PrimaryWorkEmail;
+                export["Work City"] = result[i].NTNX_WorkCity;
+            }
+            export.ExportToFile(@"C:\\Workday_Users.csv");
+
+
+            //JObject match = jsonData["young.ryu@nutanix.com"].Values<JObject>()
+            //    .Where(m => m["wvw_match_id"].Value<string>() == matchIdToFind)
+            //    .FirstOrDefault();
+
+
+            // Read the contents of CSV file
+            //var filename = @"C:\ImportUserList.txt";
+            //var contents = File.ReadAllText(filename).Split('\n');
+
+            //for(int i=0; i < contents.Length; i++)
+            //{
+            //    for (int j = 0; j < result.Count; j++)
+            //    {
+            //        if (result[j].primaryWorkEmail != null)
+            //        {
+            //            if (contents[i].ToLower() == result[j].primaryWorkEmail.ToLower())
+            //            {
+            //                export.AddRow();
+            //                export["UPN"] = contents[i];
+            //                export["Existing User?"] = "Yes";
+            //            }
+            //        }
+            //    }
+            //}
+            //export.ExportToFile(@"C:\\Workday_Users_list.csv");
+        }
+    }
+}
---
+++ ParseJSONData/WorkdayAttributes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseJSONData
+{
+    public class WorkdayAttributes
+    {
+        public string Workday_ID { get; set; }
+        public string Employee_ID { get; set; }
+        public string Preferred_Name { get; set; }
+        public string Preferred_Last_Name { get; set; }
+        public string primaryWorkEmail { get; set; }
+        public string Company { get; set; }
+        public string Cost_Center { get; set; }
+        public string primaryWorkPhone { get; set; }
+        public string Primary_Work_Mobile_Phone { get; set; }
+        public string Email_Username { get; set; }
+        public string Worker_Type { get; set; }
+        public string Manager_Name { get; set; }
+        public string Managers_Email_Username { get; set; }
+        public string Managers_PrimaryWorkEmail { get; set; }
+        public string NTNX_DirectReports { get; set; }
+        public string NTNX_BusinessUnit { get; set; }
+        public string NTNX_Division { get; set; }
+        public string NTNX_Region { get; set; }
+        public string NTNX_Status { get; set; }
+        public string NTNX_WorkCity { get; set; }
+        public string NTNX_Country { get; set; }
+        public string NTNX_ManagersMgr { get; set; }
+        public string __invalid_name__Management_Chain__Level_03 { get; set; }
+        public string __invalid_name__Management_Chain__Level_04 { get; set; }
+        public string __invalid_name__Management_Chain__Level_05 { get; set; }
+        public string __invalid_name__Management_Chain__Level_06 { get; set; }
+        public string Hire_Date { get; set; }
+    }
+}
